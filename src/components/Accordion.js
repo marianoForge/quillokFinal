562a3ccb0bb7c587fa64e9ef7b3ae5d9ed70d3f4
Mariano De Simone <desimone.mariano@gmail.com>
2021-10-28 16:29:21 -0300

    Final

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -18,62 +18,36 @@ const Accordion = ({
     <div className={projectStyles.workContent}>
       <div className={projectStyles.leftContent}>
         <div className={projectStyles.btWorks}>
-          <div
-            className={
-              isActive ? projectStyles.workSelect : projectStyles.workUnselect
-            }
-            onClick={() => setIsActive(!isActive)}
-          >
-            <div
-              className={
-                isActive ? projectStyles.highlight : projectStyles.unhighlight
-              }
-            >
-              <div
-                className={
-                  isActive
-                    ? projectStyles.arrowSelect
-                    : projectStyles.arrowUnselect
-                }
-              >
+          <div className={projectStyles.workSelect}>
+            <div className={projectStyles.highlight}>
+              <div className={projectStyles.arrowSelect}>
                 <img src={arrowMenu} alt='Arrow Menu' />
               </div>
 
-              <div
-                className={
-                  isActive
-                    ? projectStyles.textSelect
-                    : projectStyles.textUnselect
-                }
-              >
-                {title}
-              </div>
+              <div className={projectStyles.textSelect}>{title}</div>
             </div>
           </div>
         </div>
 
-        {isActive ? (
-          <div className={projectStyles.descriptionWork}>
-            <p>{content1}</p>
-            <p>{content2}</p>
-            <p>{content3}</p>
-            <p>
-              <strong>{content4}</strong>
-              <br></br>
-              {content5}
-            </p>
-          </div>
-        ) : null}
+        <div className={projectStyles.descriptionWork}>
+          <p>{content1}</p>
+          <p>{content2}</p>
+          <p>{content3}</p>
+          <p>
+            <strong>{content4}</strong>
+            <br></br>
+            {content5}
+          </p>
+        </div>
       </div>
-      {isActive ? (
-        <div className={projectStyles.rightContent}>
-          <div className={projectStyles.frameImageWork}>
-            <div className={projectStyles.imageWork}>
-              <img src={image} width='100%' alt={title} />
-            </div>
+
+      <div className={projectStyles.rightContent}>
+        <div className={projectStyles.frameImageWork}>
+          <div className={projectStyles.imageWork}>
+            <img src={image} width='100%' alt={title} />
           </div>
         </div>
-      ) : null}
+      </div>
     </div>
   );
 };
